d297a06390433c4e7609a0f53a543a6cfcf7f75a
ntsekouras <ntsekouras@outlook.com>
2023-07-20 15:22:12 +0300

    [Site Editor - Page Inspector]: Add ability to switch templates

--- packages/edit-site/src/components/sidebar-edit-mode/page-panels/edit-template.js
+++ packages/edit-site/src/components/sidebar-edit-mode/page-panels/edit-template.js
@@ -2,10 +2,14 @@
  * WordPress dependencies
  */
 import { useSelect, useDispatch } from '@wordpress/data';
-import { useMemo } from '@wordpress/element';
 import { decodeEntities } from '@wordpress/html-entities';
-import { BlockContextProvider, BlockPreview } from '@wordpress/block-editor';
-import { Button, __experimentalVStack as VStack } from '@wordpress/components';
+import {
+	DropdownMenu,
+	MenuGroup,
+	MenuItem,
+	__experimentalHStack as HStack,
+	__experimentalText as Text,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { store as coreStore } from '@wordpress/core-data';
 
@@ -13,9 +17,16 @@ import { store as coreStore } from '@wordpress/core-data';
  * Internal dependencies
  */
 import { store as editSiteStore } from '../../../store';
+import SwapTemplateButton from './swap-template-button';
+import ResetDefaultTemplate from './reset-default-template';
+
+const POPOVER_PROPS = {
+	className: 'edit-site-page-panels-edit-template__dropdown',
+	placement: 'bottom-start',
+};
 
 export default function EditTemplate() {
-	const { context, hasResolved, title, blocks } = useSelect( ( select ) => {
+	const { hasResolved, title } = useSelect( ( select ) => {
 		const { getEditedPostContext, getEditedPostType, getEditedPostId } =
 			select( editSiteStore );
 		const { getEditedEntityRecord, hasFinishedResolution } =
@@ -40,30 +51,40 @@ export default function EditTemplate() {
 
 	const { setHasPageContentFocus } = useDispatch( editSiteStore );
 
-	const blockContext = useMemo(
-		() => ( { ...context, postType: null, postId: null } ),
-		[ context ]
-	);
-
 	if ( ! hasResolved ) {
 		return null;
 	}
 
 	return (
-		<VStack>
-			<div>{ decodeEntities( title ) }</div>
-			<div className="edit-site-page-panels__edit-template-preview">
-				<BlockContextProvider value={ blockContext }>
-					<BlockPreview viewportWidth={ 1024 } blocks={ blocks } />
-				</BlockContextProvider>
-			</div>
-			<Button
-				className="edit-site-page-panels__edit-template-button"
-				variant="secondary"
-				onClick={ () => setHasPageContentFocus( false ) }
+		<HStack className="edit-site-summary-field">
+			<Text className="edit-site-summary-field__label">
+				{ __( 'Template' ) }
+			</Text>
+			<DropdownMenu
+				popoverProps={ POPOVER_PROPS }
+				focusOnMount
+				toggleProps={ { variant: 'tertiary' } }
+				label={ decodeEntities( title ) }
+				text={ decodeEntities( title ) }
+				icon={ null }
 			>
-				{ __( 'Edit template' ) }
-			</Button>
-		</VStack>
+				{ ( { onClose } ) => (
+					<>
+						<MenuGroup>
+							<MenuItem
+								onClick={ () => {
+									setHasPageContentFocus( false );
+									onClose();
+								} }
+							>
+								{ __( 'Edit template' ) }
+							</MenuItem>
+							<SwapTemplateButton onClick={ onClose } />
+						</MenuGroup>
+						<ResetDefaultTemplate onClick={ onClose } />
+					</>
+				) }
+			</DropdownMenu>
+		</HStack>
 	);
 }
